20487178073270c4d32602e3242375b9513cb92f
tkmagesh <tkmagesh77@gmail.com>
2014-07-10 10:38:30 +0530

    FP-Seed

---
+++ Day-2/FunctionalProgramming/products.js
@@ -0,0 +1,15 @@
+var products = [
+	{id:6,name :"pen", cost:60, units:70, category:2},
+	{id:2,name :"hen", cost:80, units:30, category:1},
+	{id:9,name :"ten", cost:10, units:60, category:2},
+	{id:4,name :"den", cost:90, units:20, category:1},
+	{id:7,name :"len", cost:20, units:50, category:2},
+	{id:5,name :"ken", cost:80, units:40, category:1}
+];
+
+function sort(list){
+	/*Fill in the blanks to sort the products by id*/
+}
+console.log("After sorting by id")
+sort(products);
+console.table(products);
